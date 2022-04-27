bdd1b02bcae82fcd05ea2ffde22ec806504e0fb0
techpixel <68567672+techpixel@users.noreply.github.com>
2022-04-26 19:42:20 -0500

    Add files via upload

---
+++ key_map.py
@@ -0,0 +1,18 @@
+KEY_MAP = {
+           pyxel.KEY_1: 0x1,
+           pyxel.KEY_2: 0x2,
+           pyxel.KEY_3: 0x3,
+           pyxel.KEY_4: 0xc,
+           pyxel.KEY_Q: 0x4,
+           pyxel.KEY_W: 0x5,
+           pyxel.KEY_E: 0x6,
+           pyxel.KEY_R: 0xd,
+           pyxel.KEY_A: 0x7,
+           pyxel.KEY_S: 0x8,
+           pyxel.KEY_D: 0x9,
+           pyxel.KEY_F: 0xe,
+           pyxel.KEY_Z: 0xa,
+           pyxel.KEY_X: 0,
+           pyxel.KEY_C: 0xb,
+           pyxel.KEY_V: 0xf
+           }
